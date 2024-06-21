57a789e641430100bf66a4f0c5c2a1eb1a630ae6
GirlPokiDraco <lucianogamerpro03@gmail.com>
2024-06-20 23:01:00 -0300

    Add files via upload

--- script/c15.lua
+++ script/c15.lua
@@ -14,24 +14,17 @@ function s.initial_effect(c)
     e1:SetCountLimit(1,id)
     c:RegisterEffect(e1)
     
-    --Fusion Summon Dragon-type using this card and another card
-    local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,1))
-    e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
-    e2:SetType(EFFECT_TYPE_IGNITION)
-    e2:SetRange(LOCATION_HAND+LOCATION_MZONE)
-    e2:SetTarget(s.fustg)
-    e2:SetOperation(s.fusop)
-    e2:SetCountLimit(1,{id,1})
-    c:RegisterEffect(e2)
-
-    -- Fusion Material Obligatorio
-    local e3=Effect.CreateEffect(c)
-    e3:SetType(EFFECT_TYPE_SINGLE)
-    e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-    e3:SetCode(EFFECT_FUSION_MATERIAL)
-    e3:SetValue(s.fuslimit)
-    c:RegisterEffect(e3)
+    --Fusion Summon "Raging Dragon" using this card
+	--spsummon
+	local params = {aux.FilterBoolFunction(Card.IsSetCard,0x7c9),Fusion.InHandMat,nil,nil,Fusion.ForcedHandler}
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(Fusion.SummonEffTG(table.unpack(params)))
+	e1:SetOperation(Fusion.SummonEffOP(table.unpack(params)))
+	c:RegisterEffect(e1)
 end
 
 function s.thcon(e,tp,eg,ep,ev,re,r,rp)
@@ -48,55 +41,4 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
     if c:IsRelateToEffect(e) then
         Duel.SendtoHand(c,nil,REASON_EFFECT)
     end
-end
-
-function s.fusfilter1(c,e)
-    return not c:IsImmuneToEffect(e)
-end
-
-function s.fusfilter2(c,e,tp,m,f,chkf)
-    return c:IsType(TYPE_FUSION) and c:IsRace(RACE_DRAGON) and (not f or f(c))
-        and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) and c:CheckFusionMaterial(m,nil,chkf)
-end
-
-function s.fustg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then
-        local chkf=tp
-        local mg1=Duel.GetFusionMaterial(tp):Filter(s.fusfilter1,nil,e)
-        local res=Duel.IsExistingMatchingCard(s.fusfilter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg1,nil,chkf)
-        if not res then
-            local mg2=Duel.GetMatchingGroup(s.fusfilter1,tp,LOCATION_HAND+LOCATION_MZONE,0,nil,e)
-            mg1:Merge(mg2)
-            res=Duel.IsExistingMatchingCard(s.fusfilter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg1,nil,chkf)
-        end
-        return res
-    end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
-end
-
-function s.fusop(e,tp,eg,ep,ev,re,r,rp)
-    local chkf=tp
-    local mg1=Duel.GetFusionMaterial(tp):Filter(s.fusfilter1,nil,e)
-    local sg1=Duel.GetMatchingGroup(s.fusfilter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,chkf)
-    if sg1:GetCount()>0 then
-        local mg2=Duel.GetMatchingGroup(s.fusfilter1,tp,LOCATION_HAND+LOCATION_MZONE,0,nil,e)
-        mg1:Merge(mg2)
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-        local tg=sg1:Select(tp,1,1,nil)
-        local tc=tg:GetFirst()
-        if tc then
-            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FMATERIAL)
-            local mat1=Duel.SelectFusionMaterial(tp,tc,mg1,nil,chkf)
-            tc:SetMaterial(mat1)
-            Duel.SendtoGrave(mat1,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
-            Duel.BreakEffect()
-            Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
-            tc:CompleteProcedure()
-        end
-    end
-end
-
-function s.fuslimit(e,c)
-    if not c then return false end
-    return c:IsRace(RACE_DRAGON)
 end
