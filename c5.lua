9ff4918a4be61adaf3e3ac199feb21aff98aaef0
GirlPokiDraco <lucianogamerpro03@gmail.com>
2024-06-21 19:23:54 -0300

    Add files via upload

---
+++ c5.lua
@@ -0,0 +1,84 @@
+-- Dragón Furioso Audaz
+local s,id=GetID()
+
+function s.initial_effect(c)
+    -- Efecto 1: Invocar 2 copias de esta carta desde el Deck o Cementerio y cambiar su nivel a 4
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.spsumtg)
+	e1:SetOperation(s.spsumop)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+
+    -- Efecto 2: Descartar una carta al azar de la mano del oponente al ser desacoplada como material XYZ
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_DECKDES)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_BE_MATERIAL)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCondition(s.desmaterial)
+	e3:SetTarget(s.destarget)
+	e3:SetOperation(s.desoperation)
+	c:RegisterEffect(e3)
+end
+
+function s.spsumfilter(c,e,tp)
+	return c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+
+function s.spsumtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>1
+		and Duel.IsExistingMatchingCard(s.spsumfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,2,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,LOCATION_DECK+LOCATION_GRAVE)
+end
+
+function s.spsumop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=1 then return end
+	local g=Duel.SelectMatchingCard(tp,s.spsumfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,2,2,nil,e,tp)
+	if #g>0 then
+		for tc in aux.Next(g) do
+			Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP)
+			-- Cambiar el nivel de los monstruos invocados a 4
+			local e1=Effect.CreateEffect(tc)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+			e1:SetRange(LOCATION_MZONE)
+			e1:SetCode(EFFECT_CHANGE_LEVEL)
+			e1:SetValue(4)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e1,true)
+		end
+		Duel.SpecialSummonComplete()
+	end
+end
+
+function s.desmaterial(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c and c:IsPreviousLocation(LOCATION_MZONE) then
+		Debug.Message("Desmaterial triggered: ", c:GetName())
+	end
+	return r==REASON_XYZ and c and c:IsPreviousLocation(LOCATION_MZONE) and c:IsSummonType(SUMMON_TYPE_XYZ)
+end
+
+function s.destarget(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,0,LOCATION_HAND,1,nil,REASON_EFFECT) end
+	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,1-tp,1)
+end
+
+function s.desoperation(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_HAND)
+	if #g>0 then
+		Duel.ConfirmCards(tp,g)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
+		local sg=g:RandomSelect(tp,1)
+		Duel.SendtoGrave(sg,REASON_EFFECT+REASON_DISCARD)
+		Duel.ShuffleHand(1-tp)
+	end
+end
