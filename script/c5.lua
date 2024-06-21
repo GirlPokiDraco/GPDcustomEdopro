e3f10fae466e44b13838db0c52eb154ddafae1b7
GirlPokiDraco <lucianogamerpro03@gmail.com>
2024-06-21 20:51:47 -0300

    Add files via upload

--- script/c5.lua
+++ script/c5.lua
@@ -1,10 +1,9 @@
--- Dragón Furioso Audaz
 local s,id=GetID()
 function s.initial_effect(c)
     -- Efecto de Descarte para Negar
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_NEGATE)
+	e1:SetCategory(CATEGORY_DISABLE)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_CHAINING)
 	e1:SetRange(LOCATION_HAND)
@@ -17,9 +16,11 @@ function s.initial_effect(c)
 end
 
 function s.negateCondition(e,tp,eg,ep,ev,re,r,rp)
-    -- Check if it's the opponent's effect that adds cards from their Deck to their hand
-    return rp~=tp and Duel.GetCurrentChain()>1 and re:IsHasType(EFFECT_TYPE_ACTIVATE) 
-        and Duel.IsChainNegatable(ev) and re:GetHandler():IsType(TYPE_MONSTER+TYPE_SPELL+TYPE_TRAP)
+    -- Verificar si la activación del adversario añade cartas desde su Deck a su mano
+    return rp~=tp and Duel.IsChainDisablable(ev) and Duel.GetCurrentChain()>1 
+        and re:GetHandler():IsType(TYPE_MONSTER+TYPE_SPELL+TYPE_TRAP) 
+        and re:IsHasType(EFFECT_TYPE_ACTIVATE) 
+        and (re:IsActiveType(TYPE_SPELL) or re:IsActiveType(TYPE_MONSTER))
 end
 
 function s.negateCost(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -29,15 +30,9 @@ end
 
 function s.negateTarget(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
 end
 
 function s.negateOperation(e,tp,eg,ep,ev,re,r,rp)
 	Duel.NegateActivation(ev)
-    -- Get the cards added by your opponent's effect
-    local addedCards=Duel.GetOperatedGroup():Filter(Card.IsLocation,nil,LOCATION_HAND+LOCATION_DECK)
-    if #addedCards>0 then
-        -- Send the added cards to the Graveyard
-        Duel.SendtoGrave(addedCards,REASON_EFFECT)
-    end
 end
