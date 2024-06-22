aff5d6228a0463971fa1d3cb50221238c3c68bf3
GirlPokiDraco <lucianogamerpro03@gmail.com>
2024-06-21 21:00:08 -0300

    Add files via upload

--- script/c5.lua
+++ script/c5.lua
@@ -1,38 +1,37 @@
+-- Dragón Furioso Audaz
 local s,id=GetID()
 function s.initial_effect(c)
-    -- Efecto de Descarte para Negar
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_DISABLE)
-	e1:SetType(EFFECT_TYPE_QUICK_O)
-	e1:SetCode(EVENT_CHAINING)
-	e1:SetRange(LOCATION_HAND)
-	e1:SetCountLimit(1,id)
-	e1:SetCondition(s.negateCondition)
-	e1:SetCost(s.negateCost)
-	e1:SetTarget(s.negateTarget)
-	e1:SetOperation(s.negateOperation)
-	c:RegisterEffect(e1)
+    -- Efecto 1: Negar la adición de cartas desde el Deck del adversario a su mano
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_DISABLE)
+    e1:SetType(EFFECT_TYPE_QUICK_O)
+    e1:SetCode(EVENT_CHAINING)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCountLimit(1,id)
+    e1:SetCondition(s.discon)
+    e1:SetCost(s.discost)
+    e1:SetTarget(s.distg)
+    e1:SetOperation(s.disop)
+    c:RegisterEffect(e1)
 end
 
-function s.negateCondition(e,tp,eg,ep,ev,re,r,rp)
-    -- Verificar si la activación del adversario añade cartas desde su Deck a su mano
-    return rp~=tp and Duel.IsChainDisablable(ev) and Duel.GetCurrentChain()>1 
-        and re:GetHandler():IsType(TYPE_MONSTER+TYPE_SPELL+TYPE_TRAP) 
-        and re:IsHasType(EFFECT_TYPE_ACTIVATE) 
-        and (re:IsActiveType(TYPE_SPELL) or re:IsActiveType(TYPE_MONSTER))
+function s.discon(e,tp,eg,ep,ev,re,r,rp)
+    local ex,cg,ct,cp,cv=Duel.GetOperationInfo(ev,CATEGORY_TOHAND)
+    return ex and cp~=tp and Duel.IsChainDisablable(ev)
 end
 
-function s.negateCost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsDiscardable() end
-	Duel.SendtoGrave(e:GetHandler(),REASON_COST+REASON_DISCARD)
+function s.discost(e,tp,eg,ep,ev,re,r,rp,chk)
+    local c=e:GetHandler()
+    if chk==0 then return c:IsDiscardable() end
+    Duel.SendtoGrave(c,REASON_COST+REASON_DISCARD)
 end
 
-function s.negateTarget(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
+function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return not re:GetHandler():IsStatus(STATUS_DISABLED) end
+    Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
 end
 
-function s.negateOperation(e,tp,eg,ep,ev,re,r,rp)
-	Duel.NegateActivation(ev)
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.NegateEffect(ev)
 end
