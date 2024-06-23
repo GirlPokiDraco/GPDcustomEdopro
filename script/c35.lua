7f7223d3b6ce0db8b38ded062e0322cc3c060df2
GirlPokiDraco <lucianogamerpro03@gmail.com>
2024-06-22 21:37:21 -0300

    Add files via upload

--- script/c35.lua
+++ script/c35.lua
@@ -1,16 +1,33 @@
---Raging Dragon Secret Fusion
-local s,id=GetID()
+-- Raging Dragon Secret Fusion
+local s, id = GetID()
+
 function s.initial_effect(c)
-	--Activate
-	local e1=Fusion.CreateSummonEff{handler=c,matfilter=Fusion.OnFieldMat,extrafil=s.fextra,extratg=s.extratg}
-	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
-	c:RegisterEffect(e1)
+    -- Activate
+    local e1 = Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id, 0))
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetHintTiming(0, TIMINGS_CHECK_MONSTER_E)
+    e1:SetCondition(s.ntcon)
+    e1:SetTarget(s.target)
+    e1:SetOperation(s.operation)
+    e1:SetCost(s.cost) -- Aquí se establece el costo
+    c:RegisterEffect(e1)
 end
-function s.fextra(e,tp,mg)
-	return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsFaceup),tp,0,LOCATION_ONFIELD,nil)
+
+function s.fextra(e, tp, mg)
+    return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsFaceup), tp, 0, LOCATION_ONFIELD, nil)
 end
-function s.extratg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if e:IsHasType(EFFECT_TYPE_ACTIVATE) then
-		Duel.SetChainLimit(aux.FALSE)
-	end
+
+function s.extratg(e, tp, eg, ep, ev, re, r, rp, chk)
+    if e:IsHasType(EFFECT_TYPE_ACTIVATE) then
+        Duel.SetChainLimit(aux.FALSE)
+    end
+end
+
+function s.cost(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then return Duel.CheckLPCost(tp, 1000) end -- Costo de 1000 LP
+    Duel.PayLPCost(tp, 1000)
 end
