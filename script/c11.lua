63e1418f483b3f4147d0422d2da7d073ef41979d
GirlPokiDraco <lucianogamerpro03@gmail.com>
2024-06-23 18:56:33 -0300

    Update c11.lua

--- script/c11.lua
+++ script/c11.lua
@@ -1,7 +1,7 @@
 -- Raging Dragon Fusion
 local s,id=GetID()
 function s.initial_effect(c)
-	-- Activate
+	-- Activate: Fusion Summon from Deck
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -10,10 +10,20 @@ function s.initial_effect(c)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
+	-- Graveyard effect: Banish to Fusion Summon from Field
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCost(aux.bfgcost)
+	e2:SetTarget(s.gytarget)
+	e2:SetOperation(s.gyactivate)
+	c:RegisterEffect(e2)
 end
 
 s.listed_series={0x7c9}
 
+-- Effect 1: Fusion Summon from Deck
 function s.filter0(c)
 	return c:IsAbleToGrave()
 end
@@ -37,6 +47,40 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local chkf=tp
 	local mg=Duel.GetMatchingGroup(s.filter1,tp,LOCATION_DECK,0,nil,e)
 	local sg=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg,nil,chkf)
+	if #sg>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local tc=sg:Select(tp,1,1,nil):GetFirst()
+		if tc then
+			local mat=Duel.SelectFusionMaterial(tp,tc,mg,nil,chkf)
+			tc:SetMaterial(mat)
+			Duel.SendtoGrave(mat,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
+			Duel.BreakEffect()
+			Duel.SpecialSummon(tc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
+			tc:CompleteProcedure()
+		end
+	end
+end
+
+-- Effect 2: Banish from Graveyard to Fusion Summon from Field
+function s.gyfilter1(c,e)
+	return not c:IsImmuneToEffect(e)
+end
+function s.gyfilter2(c,e,tp,m,f,chkf)
+	return c:IsType(TYPE_FUSION) and c:IsSetCard(0x7c9) and (not f or f(c)) 
+		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) and c:CheckFusionMaterial(m,nil,chkf)
+end
+function s.gytarget(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local chkf=tp
+		local mg=Duel.GetMatchingGroup(s.gyfilter1,tp,LOCATION_MZONE,0,nil,e)
+		return Duel.IsExistingMatchingCard(s.gyfilter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg,nil,chkf)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function s.gyactivate(e,tp,eg,ep,ev,re,r,rp)
+	local chkf=tp
+	local mg=Duel.GetMatchingGroup(s.gyfilter1,tp,LOCATION_MZONE,0,nil,e)
+	local sg=Duel.GetMatchingGroup(s.gyfilter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg,nil,chkf)
 	if #sg>0 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 		local tc=sg:Select(tp,1,1,nil):GetFirst()
