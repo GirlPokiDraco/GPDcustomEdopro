db80398f30c8f779e04524e55a5075f9a5680017
GirlPokiDraco <lucianogamerpro03@gmail.com>
2024-06-22 21:44:40 -0300

    Add files via upload

--- script/c35.lua
+++ script/c35.lua
@@ -1,53 +1,16 @@
--- Raging Dragon Secret Fusion
-local s, id = GetID()
-
+--Raging Dragon Secret Fusion
+local s,id=GetID()
 function s.initial_effect(c)
-    -- Activate
-    local e1 = Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id, 0))
-    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e1:SetType(EFFECT_TYPE_ACTIVATE)
-    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-    e1:SetCode(EVENT_FREE_CHAIN)
-    e1:SetHintTiming(0, TIMINGS_CHECK_MONSTER_E)
-    e1:SetCondition(s.ntcon)
-    e1:SetTarget(s.target)
-    e1:SetOperation(s.operation)
-    e1:SetCost(s.cost)
-    c:RegisterEffect(e1)
+	--Activate
+	local e1=Fusion.CreateSummonEff{handler=c,matfilter=Fusion.OnFieldMat,extrafil=s.fextra,extratg=s.extratg}
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
+	c:RegisterEffect(e1)
 end
-
-function s.fextra(e, tp, mg)
-    return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsFaceup), tp, 0, LOCATION_ONFIELD, nil)
+function s.fextra(e,tp,mg)
+	return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsFaceup),tp,0,LOCATION_ONFIELD,nil)
 end
-
-function s.extratg(e, tp, eg, ep, ev, re, r, rp, chk)
-    if e:IsHasType(EFFECT_TYPE_ACTIVATE) then
-        Duel.SetChainLimit(aux.FALSE)
-    end
-end
-
-function s.cost(e, tp, eg, ep, ev, re, r, rp, chk)
-    if chk == 0 then return Duel.CheckLPCost(tp, 1000) end
-    Duel.PayLPCost(tp, 1000)
-end
-
-function s.ntcon(e)
-    return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0
-end
-
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-        and Duel.IsExistingMatchingCard(s.fextra,tp,0,LOCATION_MZONE,1,nil) end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
-end
-
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
-    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-    local g=Duel.GetMatchingGroup(s.fextra,tp,0,LOCATION_MZONE,nil)
-    if #g>0 then
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-        local sg=g:Select(tp,1,1,nil)
-        Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
-    end
+function s.extratg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if e:IsHasType(EFFECT_TYPE_ACTIVATE) then
+		Duel.SetChainLimit(aux.FALSE)
+	end
 end
