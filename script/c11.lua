27eafd7ee9b590bcabff88756d3798708849d727
GirlPokiDraco <lucianogamerpro03@gmail.com>
2024-06-23 02:22:08 -0300

    Add files via upload

--- script/c11.lua
+++ script/c11.lua
@@ -1,8 +1,10 @@
 -- Raging Dragon Fusion
 local s,id=GetID()
 function s.initial_effect(c)
-	c:RegisterEffect(Fusion.CreateSummonEff(c,aux.FilterBoolFunction(Card.IsSetCard,0x7c9)))
-	--salvage
+	-- Fusion Summon effect
+	local e1 = Fusion.CreateSummonEff(c, aux.FilterBoolFunction(Card.IsSetCard, 0x7c9), Fusion.UseCardMaterialFromDeck)
+	c:RegisterEffect(e1)
+	-- Salvage effect
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_TOHAND)
 	e2:SetDescription(aux.Stringid(id,1))
@@ -14,19 +16,27 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 s.listed_series={0x7c9}
+
+-- Filter for archetype monsters in the Graveyard and on the field for Salvage cost
 function s.thfilter(c)
 	return c:IsSetCard(0x7c9) and c:IsMonster() and c:IsAbleToRemoveAsCost() and aux.SpElimFilter(c,true)
 end
+
+-- Salvage cost
 function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE|LOCATION_MZONE,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE+LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE|LOCATION_MZONE,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE+LOCATION_MZONE,0,1,1,nil)
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
+
+-- Salvage target
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToHand() end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
 end
+
+-- Salvage operation
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) then
