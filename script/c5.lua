7ffba5cc7374d338844d5fdcc4cabcd058d105f1
GirlPokiDraco <lucianogamerpro03@gmail.com>
2024-06-23 22:42:34 -0300

    Update c5.lua

--- script/c5.lua
+++ script/c5.lua
@@ -13,10 +13,18 @@ function s.initial_effect(c)
     e1:SetCost(s.discost)
     e1:SetOperation(s.disop)
     c:RegisterEffect(e1)
+    if not s.global_check then
+        s.global_check=true
+        local ge1=Effect.CreateEffect(c)
+        ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+        ge1:SetCode(EVENT_PHASE_START+PHASE_BATTLE+PHASE_MAIN1+PHASE_MAIN2+PHASE_END)
+        ge1:SetOperation(s.check_op)
+        Duel.RegisterEffect(ge1,0)
+    end
 end
 
 function s.discon(e,tp,eg,ep,ev,re,r,rp)
-    return Duel.GetTurnPlayer()~=tp and Duel.GetCurrentPhase()~=PHASE_DRAW and Duel.IsChainDisablable(ev) and e:GetHandler():IsDiscardable()
+    return Duel.GetTurnPlayer()~=tp and e:GetHandler():IsDiscardable()
 end
 
 function s.discost(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -25,6 +33,19 @@ function s.discost(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 
 function s.disop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
+end
+
+function s.check_op(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.GetFlagEffect(tp,id)==0 then return end
+    Duel.ResetFlagEffect(tp,id)
+    local g=Duel.GetFieldGroup(tp,LOCATION_HAND,0)
+    if #g>0 then
+        Duel.ConfirmCards(1-tp,g)
+        if g:IsExists(Card.IsType,1,nil,TYPE_MONSTER) then
+            Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
+        end
+    end
     local e1=Effect.CreateEffect(e:GetHandler())
     e1:SetType(EFFECT_TYPE_FIELD)
     e1:SetCode(EFFECT_CANNOT_ADD_DECK_MONSTER)
