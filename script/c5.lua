21a7165592ba91960745d5ab5d9d75de7cc579ac
GirlPokiDraco <lucianogamerpro03@gmail.com>
2024-06-23 22:37:13 -0300

    Update c5.lua

--- script/c5.lua
+++ script/c5.lua
@@ -1,23 +1,22 @@
 -- Dragón Furioso Audaz
 local s,id=GetID()
 function s.initial_effect(c)
-    -- Efecto: Negar activaciones de efectos que añaden o roban del Deck
+    -- Efecto: Descartar esta carta para evitar que el oponente añada o robe cartas fuera de su fase de robo durante este turno
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_DISABLE)
     e1:SetType(EFFECT_TYPE_QUICK_O)
-    e1:SetCode(EVENT_CHAINING)
+    e1:SetCode(EVENT_FREE_CHAIN)
     e1:SetRange(LOCATION_HAND)
     e1:SetCountLimit(1,id)
     e1:SetCondition(s.discon)
     e1:SetCost(s.discost)
-    e1:SetTarget(s.distg)
     e1:SetOperation(s.disop)
     c:RegisterEffect(e1)
 end
 
 function s.discon(e,tp,eg,ep,ev,re,r,rp)
-    return Duel.IsChainDisablable(ev) and e:GetHandler():IsDiscardable()
+    return Duel.GetTurnPlayer()~=tp and Duel.GetCurrentPhase()~=PHASE_DRAW and Duel.IsChainDisablable(ev) and e:GetHandler():IsDiscardable()
 end
 
 function s.discost(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -25,11 +24,15 @@ function s.discost(e,tp,eg,ep,ev,re,r,rp,chk)
     Duel.SendtoGrave(e:GetHandler(),REASON_COST+REASON_DISCARD)
 end
 
-function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return true end
-    Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
-end
-
 function s.disop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.NegateEffect(ev)
+    local e1=Effect.CreateEffect(e:GetHandler())
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(EFFECT_CANNOT_ADD_DECK_MONSTER)
+    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e1:SetTargetRange(1,0)
+    e1:SetReset(RESET_PHASE+PHASE_END)
+    Duel.RegisterEffect(e1,tp)
+    local e2=e1:Clone()
+    e2:SetCode(EFFECT_CANNOT_DRAW)
+    Duel.RegisterEffect(e2,tp)
 end
