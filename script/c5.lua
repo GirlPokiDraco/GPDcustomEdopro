d4acbf61d46adfcba293b7a71690f120f3cb2769
GirlPokiDraco <lucianogamerpro03@gmail.com>
2024-06-23 23:47:14 -0300

    Update c5.lua

--- script/c5.lua
+++ script/c5.lua
@@ -1,7 +1,7 @@
 -- Dragón Furioso Audaz
 local s,id=GetID()
 function s.initial_effect(c)
-    -- Efecto 1: Descartar esta carta para evitar que el oponente añada o robe cartas fuera de su fase de robo durante este turno
+    -- Efecto: Descartar esta carta para evitar que el oponente añada o robe cartas fuera de su fase de robo durante este turno
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_DISABLE)
@@ -13,17 +13,6 @@ function s.initial_effect(c)
     e1:SetCost(s.cost)
     e1:SetOperation(s.operation)
     c:RegisterEffect(e1)
-
-    -- Efecto 2: Invocación Especial desde el Cementerio como nivel 4 una vez por turno
-    local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,1))
-    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e2:SetType(EFFECT_TYPE_IGNITION)
-    e2:SetRange(LOCATION_GRAVE)
-    e2:SetCountLimit(1,id+100)
-    e2:SetTarget(s.sptg2)
-    e2:SetOperation(s.spop2)
-    c:RegisterEffect(e2)
 end
 
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -32,48 +21,23 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
-    -- Efecto para evitar que el oponente añada cartas a la mano
     local e1=Effect.CreateEffect(e:GetHandler())
     e1:SetType(EFFECT_TYPE_FIELD)
     e1:SetCode(EFFECT_CANNOT_TO_HAND)
-    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-    e1:SetTargetRange(0,1)
-    e1:SetTarget(s.handlimit)
+    e1:SetTargetRange(LOCATION_DECK,LOCATION_DECK)
     e1:SetReset(RESET_PHASE+PHASE_END)
     Duel.RegisterEffect(e1,tp)
-    -- Efecto para evitar que el oponente robe cartas
     local e2=Effect.CreateEffect(e:GetHandler())
     e2:SetType(EFFECT_TYPE_FIELD)
-    e2:SetCode(EFFECT_CANNOT_DRAW)
     e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-    e2:SetTargetRange(0,1)
+    e2:SetCode(EFFECT_CANNOT_DRAW)
+    e2:SetTargetRange(1,1)
     e2:SetReset(RESET_PHASE+PHASE_END)
     Duel.RegisterEffect(e2,tp)
-end
-
-function s.handlimit(e,c,tp,re)
-    return not re:IsHasType(EFFECT_TYPE_DRAW)
-end
-
-function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-        and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
-end
-
-function s.spop2(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsRelateToEffect(e) then
-        if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP_DEFENSE)~=0 then
-            -- Cambiar el nivel del monstruo invocado a 4
-            local e1=Effect.CreateEffect(c)
-            e1:SetType(EFFECT_TYPE_SINGLE)
-            e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-            e1:SetRange(LOCATION_MZONE)
-            e1:SetCode(EFFECT_CHANGE_LEVEL)
-            e1:SetValue(4)
-            e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-            c:RegisterEffect(e1,true)
-        end
-    end
+    local e3=Effect.CreateEffect(e:GetHandler())
+    e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+    e3:SetDescription(aux.Stringid(id,1))
+    e3:SetReset(RESET_PHASE+PHASE_END)
+    e3:SetTargetRange(1,1)
+    Duel.RegisterEffect(e3,tp)
 end
