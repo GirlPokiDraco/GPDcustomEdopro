d83080357ed4906abcfb350eea41879c925e899c
GirlPokiDraco <lucianogamerpro03@gmail.com>
2024-06-27 19:16:08 -0300

    Update c22.lua

--- script/c22.lua
+++ script/c22.lua
@@ -1,75 +1,87 @@
 --Void Raging Dragon
 local s,id=GetID()
 function s.initial_effect(c)
-	--xyz summon
-	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x7c9),4,3,s.ovfilter,aux.Stringid(id,0),3,s.xyzop)
-	c:EnableReviveLimit()
-	--Negate activation
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
-	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
-	e1:SetType(EFFECT_TYPE_QUICK_O)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCode(EVENT_CHAINING)
-	e1:SetCondition(s.condition1)
-	e1:SetCost(aux.dxmcostgen(1,1,nil))
-	e1:SetTarget(s.target1)
-	e1:SetOperation(s.operation1)
-	c:RegisterEffect(e1,false,REGISTER_FLAG_DETACH_XMAT)
-	--Negate Normal Summon
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_DISABLE_SUMMON+CATEGORY_DESTROY)
-	e2:SetType(EFFECT_TYPE_QUICK_O)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCode(EVENT_SUMMON)
-	e2:SetCondition(s.condition2)
-	e2:SetCost(aux.dxmcostgen(1,1,nil))
-	e2:SetTarget(s.target2)
-	e2:SetOperation(s.operation2)
-	c:RegisterEffect(e2,false,REGISTER_FLAG_DETACH_XMAT)
-	--Negate Special Summon
-	local e3=e2:Clone()
-	e3:SetDescription(aux.Stringid(id,1))
-	e3:SetCode(EVENT_SPSUMMON)
-	c:RegisterEffect(e3,false,REGISTER_FLAG_DETACH_XMAT)
-	aux.DoubleSnareValidity(c,LOCATION_MZONE)
+    --xyz summon
+    Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(s.ffilter),4,3,s.ovfilter,aux.Stringid(id,0),3,s.xyzop)
+    c:EnableReviveLimit()
+    --Attach 1 "Raging Dragon" monster from GY when Xyz Summoned
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,1))
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e1:SetCondition(s.attachcon)
+    e1:SetTarget(s.attachtg)
+    e1:SetOperation(s.attachop)
+    c:RegisterEffect(e1)
+    --Negate activation
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,2))
+    e2:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+    e2:SetType(EFFECT_TYPE_QUICK_O)
+    e2:SetCode(EVENT_CHAINING)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1,id)
+    e2:SetCondition(s.negcon)
+    e2:SetCost(s.negcost)
+    e2:SetTarget(s.negtg)
+    e2:SetOperation(s.negop)
+    c:RegisterEffect(e2)
 end
+
+function s.ffilter(c,fc,sumtype,tp)
+    return c:IsSetCard(0x7c9,fc,sumtype,tp) and not c:IsCode(id)
+end
+
 function s.ovfilter(c,tp,lc)
-	return c:IsFaceup() and c:GetRank()==4 and c:IsRace(RACE_DRAGON,lc,SUMMON_TYPE_XYZ,tp)
+    return c:IsFaceup() and c:GetRank()==4 and c:IsRace(RACE_DRAGON,lc,SUMMON_TYPE_XYZ,tp)
 end
+
 function s.xyzop(e,tp,chk)
-	if chk==0 then return Duel.GetFlagEffect(tp,id)==0 end
-	Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
-	return true
+    if chk==0 then return Duel.GetFlagEffect(tp,id)==0 end
+    Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
+    return true
 end
-function s.condition1(e,tp,eg,ep,ev,re,r,rp)
-	return not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED)
-		and re:IsHasType(EFFECT_TYPE_ACTIVATE) and Duel.IsChainNegatable(ev)
+
+function s.attachcon(e,tp,eg,ep,ev,re,r,rp)
+    return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ)
 end
-function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
-	local rc=re:GetHandler()
-	if rc:IsDestructable() and rc:IsRelateToEffect(re) then
-		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
-	end
+
+function s.filter(c)
+    return c:IsSetCard(0x7c9) and c:IsType(TYPE_MONSTER)
 end
-function s.operation1(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
-		Duel.Destroy(eg,REASON_EFFECT)
-	end
+
+function s.attachtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,nil) end
 end
-function s.condition2(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetCurrentChain()==0
+
+function s.attachop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+    local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil)
+    if #g>0 then
+        Duel.Overlay(c,g)
+    end
 end
-function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_DISABLE_SUMMON,eg,#eg,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,#eg,0,0)
+
+function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+    return rp==1-tp and Duel.IsChainNegatable(ev)
 end
-function s.operation2(e,tp,eg,ep,ev,re,r,rp)
-	Duel.NegateSummon(eg)
-	Duel.Destroy(eg,REASON_EFFECT)
+
+function s.negcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,2,REASON_COST) end
+    e:GetHandler():RemoveOverlayCard(tp,2,2,REASON_COST)
+end
+
+function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+    if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+        Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+    end
+end
+
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+        Duel.Destroy(eg,REASON_EFFECT)
+    end
 end
